92ca0e3b74078b5671894c57b7eaca17fd69038e
lklcentennial <klleead@connect.ust.hk>
2023-06-21 11:54:39 -0400

    fifth commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+Instruction to open the application: 
+
+1. git pull the repository to local directory
+2. install necessary modules/dependencies
+3. npm start
--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: ""
+    url: "mongodb+srv://kennyli0023:rjX3VBdHEoDHozUi@cluster0.sj8da2q.mongodb.net/?retryWrites=true&w=majority"
 }
